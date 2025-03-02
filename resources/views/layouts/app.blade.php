39dbf97407e130270cd3153dad546c120279390c
antoniosirait29 <antoniosirait29@gmail.com>
2025-03-02 19:43:20 +0700

    adding component navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,31 +9,32 @@
     <meta name="description" content="@yield('meta_description', 'Mantis is made using Bootstrap 5 design framework. Download the free admin template & use it for your project.')">
     <meta name="keywords" content="@yield('meta_keywords', 'Mantis, Dashboard UI Kit, Bootstrap 5, Admin Template, Admin Dashboard, CRM, CMS, Bootstrap Admin Template')">
     <meta name="author" content="CodedThemes">
-    
+
     <!-- [Favicon] icon -->
     <link rel="icon" href="{{ asset('template/mantis/dist/assets/images/favicon.svg') }}" type="image/x-icon">
-    
+
     <!-- [Google Font] Family -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap" id="main-font-link">
-    
+
     <!-- [Tabler Icons] https://tablericons.com -->
     <link rel="stylesheet" href="{{ asset('template/mantis/dist/assets/fonts/tabler-icons.min.css') }}">
-    
+
     <!-- [Feather Icons] https://feathericons.com -->
     <link rel="stylesheet" href="{{ asset('template/mantis/dist/assets/fonts/feather.css') }}">
-    
+
     <!-- [Font Awesome Icons] https://fontawesome.com/icons -->
     <link rel="stylesheet" href="{{ asset('template/mantis/dist/assets/fonts/fontawesome.css') }}">
-    
+
     <!-- [Material Icons] https://fonts.google.com/icons -->
     <link rel="stylesheet" href="{{ asset('template/mantis/dist/assets/fonts/material.css') }}">
-    
+
     <!-- [Template CSS Files] -->
     <link rel="stylesheet" href="{{ asset('template/mantis/dist/assets/css/style.css') }}" id="main-style-link">
-    <link rel="stylesheet" href="{{ asset('template/mantis/dist/assets/css/style-preset.css') }}">  
+    <link rel="stylesheet" href="{{ asset('template/mantis/dist/assets/css/style-preset.css') }}">
 </head>
 <body data-pc-preset="preset-1" data-pc-direction="ltr" data-pc-theme="light">
     @include('../layouts.navbars.sidenav')
+    @include('../layouts.navbars.topnav')
     @yield('content')
     {{-- @guest
         @yield('content')
@@ -69,27 +70,27 @@
   <script src="{{ asset('template/mantis/dist/assets/js/fonts/custom-font.js') }}"></script>
   <script src="{{ asset('template/mantis/dist/assets/js/pcoded.js') }}"></script>
   <script src="{{ asset('template/mantis/dist/assets/js/plugins/feather.min.js') }}"></script>
-  
 
-  
-  
-  
-  
+
+
+
+
+
   <script>layout_change('light');</script>
-  
-  
-  
-  
+
+
+
+
   <script>change_box_container('false');</script>
-  
-  
-  
+
+
+
   <script>layout_rtl_change('false');</script>
-  
-  
+
+
   <script>preset_change("preset-1");</script>
-  
-  
+
+
   <script>font_change("Public-Sans");</script>
 </body>
 </html>
