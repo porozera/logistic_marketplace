1c3f6192b67fd39347eb8808c5499fd3275b984d
Gesyarhn <gesyareihan1812@gmail.com>
2025-03-10 13:11:34 +0700

    crud jenis layanan/services

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceController;
+use App\Models\Service;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +12,11 @@ Route::get('/', function () {
 Route::get('/landing-page', function () {
     return view('landing-page');
 });
+
+// Service
+Route::get('service', [ServiceController::class,'index']);
+Route::get('service-add', [ServiceController::class,'add']);
+Route::post('service-add', [ServiceController::class,'store']);
+Route::get('service/{id}/edit', [ServiceController::class, 'edit']);
+Route::put('service/{id}', [ServiceController::class, 'update']);
+Route::delete('service/{id}', [ServiceController::class, 'destroy'])->name('service.destroy');
