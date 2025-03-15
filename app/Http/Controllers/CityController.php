6984fedfee117a0fa527387c22a5310d91fba49e
Gesyarhn <gesyareihan1812@gmail.com>
2025-03-15 21:20:37 +0700

    model & controller city

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index() {
+        $cities = City::all();
+        return view('pages.admin.cities.city', ['cities' => $cities]);
+    } 
+
+    public function add() {
+        return view('pages.admin.cities.city-add');
+    }
+
+    public function store(Request $request) {
+        // dd($request->all());
+        $validated = $request->validate([
+            'id_province' => 'required|numeric',
+            'name' => 'required|max:255',
+            'code' => 'required|max:255'
+        ]);
+
+        $city = City::create($request->all());
+        return redirect('city')->with('success', 'Data Kota berhasil ditambahkan!');
+    }
 }
--- app/Models/City.php
+++ app/Models/City.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    protected $fillable = [
+        'id_province',
+        'name',
+        'postalCode'
+    ];
 }
